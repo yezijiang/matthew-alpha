8f88eef57b7e92c471a6638fabd0ee033121d531
yezijiang <maxingfight1990@163.com>
2016-10-18 17:09:18 +0800

    design pattern :decorator

---
+++ matthew-designPattern-practice/src/main/java/com.matthew.designPattern/decorator/MyClass.java
@@ -0,0 +1,12 @@
+package com.matthew.designPattern.decorator;
+
+/**
+ * Created with IntelliJ IDEA
+ * User: maxing
+ * TIME: 2016-08-18 9:31
+ */
+public class MyClass implements MyInterface{
+    public void print() {
+        System.out.print("hello!");
+    }
+}
