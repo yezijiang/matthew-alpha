1f51a229147c10e036b1cd5b321187844803f803
yezijiang <maxingfight1990@163.com>
2016-11-03 21:16:16 +0800

    Revert "io rpc 异常修正"
    This reverts commit db8682592c0ea08432bb6dfb746310f2674337e1.

--- matthew-javaBase-practice/src/main/java/com/matthew/javabase/rpc/io/RpcProvider.java
+++ matthew-javaBase-practice/src/main/java/com/matthew/javabase/rpc/io/RpcProvider.java
@@ -1,14 +1,12 @@
 package com.matthew.javabase.rpc.io;
 
-import java.io.IOException;
-
 /**
  * Created with IntelliJ IDEA
  * User: maxing
  * TIME: 2016-10-28 16:40
  */
 public class RpcProvider{
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         HelloService service = new HelloServiceImpl();
         RpcFramework.export(service,1234);
     }
